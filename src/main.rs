d1cd17187c14f566268776848e9915d352f06c07
Mateusz Pietrowiak <mateusz.pietrowiak@b-connected.io>
2023-11-30 18:15:13 +0100

    feat: playing the random sound

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod sound;
 
 fn main() {
     println!("Hello, world!");
-    // sound::play_sound("A.mp3");
-
     let letter: letters::Letter = rand::random();
-    println!("{:?}", letter);
+    println!("Random letter: {:?}", letter);
+    sound::play_sound(&format!("{:?}.mp3", letter));
 }
